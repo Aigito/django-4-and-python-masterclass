75ca0f0af1f985f2457325d63bea48eab27b051a
Dylan Pee <peedylan94@gmail.com>
2025-04-18 15:34:20 +0930

    Added more pages to first_app

--- first_app/urls.py
+++ first_app/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
 from . import views
 
-# domain.com/first_app/""
 urlpatterns = [
   path("sports",views.sports_view),
   path("finance",views.finance_view)
--- my_site/views.py
+++ my_site/views.py
@@ -2,3 +2,9 @@ from django.http.response import HttpResponse
 
 def home_page(request):
   return HttpResponse("HOME PAGE")
+
+def sports_page(request):
+  return HttpResponse("SPORTS PAGE")
+
+def finance_page(request):
+  return HttpResponse("FINANCE PAGE")
