9f1938104b5e83741ec1d86a144de5f5b64818a1
Dylan Pee <peedylan94@gmail.com>
2025-04-23 22:04:06 +0930

    Implementing variables and context in templating

--- templating_lecture/my_app/views.py
+++ templating_lecture/my_app/views.py
@@ -3,3 +3,12 @@ from django.shortcuts import render
 # Create your views here.
 def example_view(request):
   return render(request, 'my_app/example.html')
+
+def variable_view(request):
+
+  my_var = {
+    "name": "dylan",
+    "age": 30
+    }
+
+  return render(request, 'my_app/variable.html', context = my_var)
