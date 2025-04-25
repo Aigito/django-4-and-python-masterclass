9eeece76eb7e59089f136f22c3b3922b4292372c
Dylan Pee <peedylan94@gmail.com>
2025-04-25 09:47:31 +0930

    Clean slate as per lecture

--- templating_lecture/my_app/views.py
+++ templating_lecture/my_app/views.py
@@ -5,13 +5,4 @@ def example_view(request):
   return render(request, 'my_app/example.html')
 
 def variable_view(request):
-
-  my_var = {
-    "first_name": "DYLAN",
-    "last_name": "pee",
-    "some_list": [1, 2, 3],
-    "user_logged_in": True,
-    "age": 30
-  }
-
-  return render(request, 'my_app/variable.html', context = my_var)
+  return render(request, 'my_app/variable.html')
