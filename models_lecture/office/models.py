b92ad594bd907a354067de105fb9f21bb0f64305
Dylan Pee <peedylan94@gmail.com>
2025-04-27 21:55:30 +0930

    Created new field heartrate for Patient model and ran the migration

--- models_lecture/office/models.py
+++ models_lecture/office/models.py
@@ -1,10 +1,12 @@
 from django.db import models
+from django.core.validators import MaxLengthValidator, MinLengthValidator
 
 # Create your models here.
 class Patient(models.Model):
   first_name = models.CharField(max_length=30)
   last_name = models.CharField(max_length=30)
-  age = models.IntegerField()
+  age = models.IntegerField(validators = [MinLengthValidator(0), MaxLengthValidator(120)])
+  heartrate = models.IntegerField(default = 60, validators = [MinLengthValidator(1), MaxLengthValidator(300)])
 
   def __str__(self):
      return f"{self.first_name.capitalize()} {self.last_name.capitalize()} is {self.age} years old"
