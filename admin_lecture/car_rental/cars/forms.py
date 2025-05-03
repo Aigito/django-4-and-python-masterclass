043d9f0f78380480da7fcb84199af9005d31b0e4
Dylan Pee <peedylan94@gmail.com>
2025-05-03 16:16:35 +0930

    Adding custom CSS styling to Django forms

--- admin_lecture/car_rental/cars/forms.py
+++ admin_lecture/car_rental/cars/forms.py
@@ -3,5 +3,12 @@ from django import forms
 class ReviewForm(forms.Form):
   first_name = forms.CharField(label="First Name", max_length = 100)
   last_name = forms.CharField(label="Last Name", max_length = 100)
+  # to access the last_name field and apply CSS, we'd have to access the TextInput
+  # eg. widget = forms.TextInput(attrs = { "class": "myform" })
   email = forms.EmailField(label="Email")
-  review = forms.CharField(label="Please write your review here")
+  review = forms.CharField(
+    label = "Please write your review here",
+    widget = forms.Textarea(attrs={"class": "myform"})
+    # forms.Textarea --> this is how we access the field itself
+    # then we pass the CSS classes through attrs
+  )
