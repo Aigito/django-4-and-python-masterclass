f6547b7d56f5b852c3a4ca7c6524d43a44813d80
Dylan Pee <peedylan94@gmail.com>
2025-05-03 16:41:56 +0930

    Setting up ModelForm for Review model

--- admin_lecture/car_rental/cars/forms.py
+++ admin_lecture/car_rental/cars/forms.py
@@ -1,14 +1,21 @@
 from django import forms
+from .models import Review
+from django.forms import ModelForm
 
-class ReviewForm(forms.Form):
-  first_name = forms.CharField(label="First Name", max_length = 100)
-  last_name = forms.CharField(label="Last Name", max_length = 100)
-  # to access the last_name field and apply CSS, we'd have to access the TextInput
-  # eg. widget = forms.TextInput(attrs = { "class": "myform" })
-  email = forms.EmailField(label="Email")
-  review = forms.CharField(
-    label = "Please write your review here",
-    widget = forms.Textarea(attrs={"class": "myform"})
-    # forms.Textarea --> this is how we access the field itself
-    # then we pass the CSS classes through attrs
-  )
+# class ReviewForm(forms.Form):
+#   first_name = forms.CharField(label="First Name", max_length = 100)
+#   last_name = forms.CharField(label="Last Name", max_length = 100)
+#   # to access the last_name field and apply CSS, we'd have to access the TextInput
+#   # eg. widget = forms.TextInput(attrs = { "class": "myform" })
+#   email = forms.EmailField(label="Email")
+#   review = forms.CharField(
+#     label = "Please write your review here",
+#     widget = forms.Textarea(attrs={"class": "myform"})
+#     # forms.Textarea --> this is how we access the field itself
+#     # then we pass the CSS classes through attrs
+#   )
+
+class ReviewForm(ModelForm):
+  class Meta:
+    model = Review
+    fields = ["first_name", "last_name", "stars"]
