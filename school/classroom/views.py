a58bdba43bd69d08995878273dd56f206f03b82a
Dylan Pee <peedylan94@gmail.com>
2025-05-04 14:18:53 +0930

    Created Contact form and the relevant view files

--- school/classroom/views.py
+++ school/classroom/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from .forms import ContactForm
 
 # Create your views here.
 class HomeView(TemplateView):
@@ -7,3 +8,12 @@ class HomeView(TemplateView):
 
 class ThankYouView(TemplateView):
   template_name = "classroom/thank_you.html"
+
+class ContactView(TemplateView):
+  # form class connects to the relevant Form Class
+  form_class = ContactForm
+
+  # template_name connects the CBV to the relevant HTML file
+  # in FBV this was done by `return render(request, "classroom/contact.html", context = {"form": form})`
+  template_name = "classroom/contact.html"
+
