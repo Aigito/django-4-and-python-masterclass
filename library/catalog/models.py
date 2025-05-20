46c2c0f5820a42e3d13302fe8bf582418126286c
Dylan Pee <peedylan94@gmail.com>
2025-05-20 21:12:20 +0930

    Setting up models for catalog app

--- library/catalog/models.py
+++ library/catalog/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
+class Genre(models.Model):
+  name = models.CharField(max_length = 150)
+
+  def __str__(self):
+    return self.name
+
+class Book(models.Model):
+  title = models.CharField(max_length = 150)
+  # author is its own model rather than just a simple field
+
+  # on_delete = models.SET_NULL option means that when an author is deleted
+  # any book instance with this author will have author set to NULL
+  author = models.ForeignKey('Author', on_delete = models.SET_NULL)
+  summary = models.TextField(max_length = 600)
+  isbn = models.CharField("'ISBN", max_length = 13, unique = True)
+class Author(models.Model):
+  first_name = models.CharField(max_length=50)
+  last_name = models.CharField(max_length=50)
+  date_of_birth = models.DateField(null = True, blank = True)
+
+  class Meta:
+    ordering = ['last_name', 'first_name']
+
+  def get_absolute_url(self):
+      return reverse("author_detail", kwargs={"pk": self.pk})
+
+  def __str__(self):
+    return f"{self.last_name}, {self.first_name}"
