65f8a646125795524612f04c0ee96b65931e6162
Dylan Pee <peedylan94@gmail.com>
2025-05-22 21:57:52 +0930

    Add decorators to ensure that login is required to access my_view

--- library/catalog/views.py
+++ library/catalog/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Book, Author, BookInstance, Genre, Language
 from django.views.generic import CreateView, DetailView
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def index(request):
@@ -23,3 +24,7 @@ class BookCreate(CreateView):
 
 class BookDetail(DetailView):
   model = Book
+
+@login_required
+def my_view(request):
+  return render(request, 'catalog/my_view.html')
